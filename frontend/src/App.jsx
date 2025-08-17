7d6bd870d43de9a6e75f580a38f75c42820edb86
Bhuvan Sankar Andela <NrdyBhu1@gmail.com>
2025-08-17 15:30:38 +0530

    MVP completed!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,129 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
+import React from "react"
+import "./App.css"
+import axios from "axios"
+import { Line } from "react-chartjs-2"
+import Tabs from "@mui/material/Tabs"
+import Tab from "@mui/material/Tab"
+import Typography from "@mui/material/Typography"
+import Box from "@mui/material/Box"
+
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js"
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
+)
+
+function TabPanel(props) {
+	const { children, value, index, ...other } = props
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`vertical-tabpanel-${index}`}
+			aria-labelledby={`vertical-tab-${index}`}
+			{...other}>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	)
+}
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      companies: [],
-      market_data: {},
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			companies: [],
+			market_data: {},
+			value: 0,
+		}
+	}
 
-  async componentDidMount() {
-    const URI = `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/`;
-    let cached_data = {}
-    // load companies
-    let res = await axios.get(URI);
-    let companies = res.data.Companies;
-    // load data
-    companies.map(async c => {
-      let company_res = await axios.get(`${URI}${c}`)
-      cached_data[c] = company_res.data.data;
-    })
+	handleChange = (event, newValue) => {
+		this.setState({ value: newValue })
+	}
 
-    this.setState({ companies: [...companies], market_data: cached_data })
-  }
+	async componentDidMount() {
+		const URI = `http://${import.meta.env.VITE_BACKEND_HOST}:${
+			import.meta.env.VITE_BACKEND_PORT
+		}/`
+		let cached_data = {}
+		// load companies
+		let res = await axios.get(URI)
+		let companies = res.data.Companies
+		// load data
+		for (const c of companies) {
+			let company_res = await axios.get(`${URI}${c}`)
+			cached_data[c] = company_res.data.data
+		}
 
-  render() {
-    return (<div>
-        <h1>Companies</h1>
-        <ul>
-        {this.state.companies.map((c, i) => (
-          <li key={i}>
-          {c} - {JSON.stringify(this.state.market_data[0])}
-          {console.log(this.state.market_data[c])}
-          </li>
-        ))}
-        </ul>
-      </div>)
-  }
+		this.setState({ companies: companies, market_data: cached_data })
+	}
+
+	render() {
+		const { market_data, value, companies } = this.state
+
+		if (!market_data["MSFT"]) {
+			return <div> Loading...</div>
+		}
+
+		return (
+			<div>
+				<h1>Stock Market Dashboard</h1>
+				<Box
+					sx={{
+						flexGrow: 1,
+						bgcolor: "background.paper",
+						display: "flex",
+						height: 600,
+					}}>
+					<Tabs
+						orientation="vertical"
+						variant="scrollable"
+						value={value}
+						onChange={this.handleChange}
+						aria-label="Vertical tabs"
+						sx={{
+							borderRight: 1,
+							borderColor: "divider",
+							minWidth: 150,
+						}}>
+						{
+							companies.map(c => (
+								<Tab key={`tab-${c}`} label={c} />
+							))
+						}
+					</Tabs>
+
+					{
+						companies.map((c, i) => (
+							<TabPanel key={`tabpanel-${c}`} value={value} index={i}>
+								<Line width={900} height={450} data={{ labels: market_data[c].map((row) => row.mdate), datasets: [{ label: c, data: market_data[c].map((row) => row.close), borderColor: "#2196f3", borderWidth: 2 }] }} />
+							</TabPanel>
+						))
+					}
+				</Box>
+			</div>
+		)
+	}
 }
 
 export default App
